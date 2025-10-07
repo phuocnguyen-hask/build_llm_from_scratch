043d6c23a93edbb32c4d1f736c65fbcfa1e21d16
Meo <chemlink2006@gmail.com>
2025-10-07 23:14:21 +0700

    add GPT2_CONFIG

--- model/final_model.py
+++ model/final_model.py
@@ -1,6 +1,37 @@
 import torch
 import torch.nn as nn
 from .blocks import TransformerBlock
+GPT2_CONFIG = {
+  "activation_function": "gelu_new",
+  "architectures": [
+    "GPT2LMHeadModel"
+  ],
+  "attn_pdrop": 0.1,
+  "bos_token_id": 50256,
+  "embd_pdrop": 0.1,
+  "eos_token_id": 50256,
+  "initializer_range": 0.02,
+  "layer_norm_epsilon": 1e-05,
+  "model_type": "gpt2",
+  "n_ctx": 1024,
+  "n_embd": 768,
+  "n_head": 12,
+  "n_layer": 12,
+  "n_positions": 1024,
+  "resid_pdrop": 0.1,
+  "summary_activation": None,
+  "summary_first_dropout": 0.1,
+  "summary_proj_to_labels": True,
+  "summary_type": "cls_index",
+  "summary_use_proj": True,
+  "task_specific_params": {
+    "text-generation": {
+      "do_sample": True,
+      "max_length": 50
+    }
+  },
+  "vocab_size": 50257
+}
 class MyLLM(nn.Module):
     def __init__(self, cfg):
         super().__init__()
